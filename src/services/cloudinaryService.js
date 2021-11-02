222a26e6b6631b7d2c3a97604fa31046a1f49c91
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2021-11-02 13:05:35 +0800

    Update cloudinaryService.js

--- src/services/cloudinaryService.js
+++ src/services/cloudinaryService.js
@@ -6,6 +6,11 @@ cloudinary.config({
     cloud_name: config.cloudinaryCloudName,
     api_key: config.cloudinaryApiKey,
     api_secret: config.cloudinaryApiSecret,
-    //upload_preset: 'upload_to_design' //check whats this later
 });
+
+/*Existing upload presets:
+upload_forum_image
+cloudinary.uploader.upload(file.path, { upload_preset: 'upload_forum_image' })
+*/
+
 
