f5feca805d3ebb90e0edc1c1dec311d5757765c0
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2021-11-04 09:42:55 +0800

    Updated logger
    Enabled logger to do monthly rollover of log files

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,43 +1,74 @@
+const winston = require("winston");
 const { createLogger, format, transports, exitOnError } = require("winston");
 const { combine, timestamp, printf } = format;
+require("winston-daily-rotate-file");
 
 const myFormat = printf( ({ level, message, timestamp , ...metadata}) => {
-  let msg = `${timestamp} [${level}] : ${message} `;
-  if(metadata) { msg += JSON.stringify(metadata); }
-  return msg;
+    let msg = `${timestamp} [${level}] : ${message} `;
+    if(metadata) { msg += JSON.stringify(metadata); }
+    return msg;
+});
+
+const rotatingInfoTransport = new transports.DailyRotateFile({
+    filename: "logs/info-%DATE%.log",
+    datePattern: "YYYY-MM",
+    zippedArchive: true,
+    maxSize: "20m",
+    level: "info",
+    json: true
+});
+
+const rotatingErrorTransport = new transports.DailyRotateFile({
+    filename: "logs/error-%DATE%.log",
+    datePattern: "YYYY-MM",
+    zippedArchive: true,
+    maxSize: "20m",
+    level: "error",
+    json: true
+});
+
+const rotatingRequestsTransport = new transports.DailyRotateFile({
+    filename: "logs/request-%DATE%.log",
+    datePattern: "YYYY-MM",
+    zippedArchive: true,
+    maxSize: "20m",
+    json: true
 });
 
 const logger = createLogger({
-  format: combine(
-	timestamp(),
-	myFormat
-  ),
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: "logs/error.log", level: "error", json: true }),
-    new transports.File({ filename: "logs/info.log", level: "info", json: true }),
-  ]
+    format: combine(
+    timestamp(),
+    myFormat
+    ),
+    transports: [
+        new transports.Console(),
+        // new transports.File({ filename: "logs/error.log", level: "error", json: true }),
+        // new transports.File({ filename: "logs/info.log", level: "info", json: true }),
+        rotatingErrorTransport,
+        rotatingInfoTransport
+    ]
 });
 
 const requestLogger = createLogger({
-  format: combine(
-    timestamp(),
-    myFormat
-  ),
-  transports: [
-    new transports.File({ filename: "logs/request.log", json: true }),
-  ],
-  exitOnError: false,
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        // new transports.File({ filename: "logs/request.log", json: true }),
+        rotatingRequestsTransport
+    ],
+    exitOnError: false,
 });
 
 requestLogger.stream = {
-  write: function(message, encoding) {
-    logger.info(message);
-    requestLogger.info(message);
-  },
+    write: function(message, encoding) {
+        logger.info(message);
+        requestLogger.info(message);
+    },
 };
 
 module.exports = {
-  logger,
-  requestLogger,
+    logger,
+    requestLogger,
 };
